e87a4a19e9ff31739d90c426312775d946ed0d22
moling <365024424@qq.com>
2018-03-01 17:12:42 +0800

    添加bucket的操作方法

---
+++ src/models/bucket.js
@@ -0,0 +1,56 @@
+import qiniu from 'qiniu.js'
+import crypto from 'crypto-browserify'
+import config from '@/config/bucket.json'
+
+console.log(crypto)
+
+const isString = (value) => typeof value === 'string'
+const safeEncode = (str) => btoa(str).replace(/\//g, '_').replace(/\+/g, '-')
+const encodeSign = (str, key) => crypto
+  .createHmac('sha1', key)
+  .update(str)
+  .digest('base64')
+  .replace(/\//g, '_')
+  .replace(/\+/g, '-')
+
+const bucket = qiniu.bucket(config.name, { url: config.url })
+
+bucket.fetchFile = (filename) => bucket.getFile(`${filename}?r=${Math.random()}`)
+
+/**
+ * 通过密码获取七牛数据桶的密钥
+ * @param  String password 密码
+ * @return Promise         包含密钥的Promise
+ */
+bucket.getKeys = (password) => {
+  if (!isString(password)) {
+    throw new TypeError('Password must be a string')
+  }
+  return bucket.fetchFile(`secret-${password}.json`).then(body => JSON.parse(body))
+}
+
+/**
+ * 通过密码获取七牛数据桶的上传令牌
+ * @param  String|Object password   密码或密钥
+ * @param  String        scope      上传标识
+ * @return Promise                  上传令牌
+ */
+bucket.fetchPutToken = (password, scope = null) => {
+  return (isString(password) ? bucket.getKeys(password) : Promise.resolve(password))
+    .then(keys => {
+      if (!(keys && keys.ak && keys.sk)) {
+        throw new TypeError('keys.ak and keys.sk are required')
+      }
+      const options = {
+        scope: scope ? `${config.name}:${scope}` : config.name,
+        deadline: Math.floor(Date.now() / 1000) + 3600
+      }
+
+      const signture = safeEncode(JSON.stringify(options))
+      const encodeDigest = encodeSign(signture, keys.sk)
+      const token = `${keys.ak}:${encodeDigest}:${signture}`
+      return token
+    })
+}
+
+export default bucket
