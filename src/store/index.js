52c7af39a28acce1f48969b3adfe47950ee86ca4
moling <365024424@qq.com>
2018-03-02 15:37:52 +0800

    导入vuex添加store

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import bucket from '@/models/bucket'
+import Model from '@/models/Model'
+
+Vue.use(Vuex)
+
+const configModel = new Model('config')
+
+const store = new Vuex.Store({
+  state: {
+    keys: {},
+    config: {}
+  },
+  getters: {
+    keys: state => state.keys,
+    config: state => state.config
+  },
+  mutations: {
+    setKeys (state, keys) {
+      state.keys = keys
+    },
+    setConfig (state, config) {
+      state.config = config
+    }
+  },
+  actions: {
+    getKeys ({ commit }, password) {
+      return bucket.getKeys(password).then(keys => commit('setKeys', keys))
+    },
+    getConfig ({ commit }) {
+      return configModel.fetch().then(config => commit('setConfig', config))
+    }
+  },
+  strict: process.env.NODE_ENV !== 'production'
+})
+
+export default store
