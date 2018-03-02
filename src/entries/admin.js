b2ff6a706179c274018a635a1fdd2dd5839b106f
moling <365024424@qq.com>
2018-03-02 15:38:51 +0800

    改写进入admin页面的逻辑

--- src/entries/admin.js
+++ src/entries/admin.js
@@ -1,12 +1,41 @@
 import Vue from 'vue'
+import swal from 'sweetalert2'
 import App from '@/App'
+import store from '@/store'
 import router from '@/router/admin'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+const actions = ['getConfig']
+swal({
+  title: 'input password',
+  type: 'info',
+  input: 'password',
+  confirmButtonText: 'Login',
+  allowOutsideClick: false,
+  inputValidator: value => {
+    return !value && 'password cannot be empty'
+  }
 })
+  .then(({ value: password }) => store.dispatch('getKeys', password))
+  .then(keys => Promise.all(actions.map(action => store.dispatch(action))))
+  .then(() => {
+    swal({
+      type: 'success',
+      title: 'welcome back',
+      timer: 1000
+    })
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      store,
+      router,
+      render: h => h(App)
+    })
+  })
+  .catch(() => {
+    swal({
+      title: 'password wrong',
+      type: 'error'
+    })
+  })
