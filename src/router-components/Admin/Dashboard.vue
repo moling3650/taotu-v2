753e0867a2138ee6880ed997f9392241a30c04f6
moling <365024424@qq.com>
2018-03-05 18:06:48 +0800

    添加Dashboard组件

---
+++ src/router-components/Admin/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">Dashboard</div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
