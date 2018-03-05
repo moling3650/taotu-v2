66b1f6cb191290cab587ed0dd268cfcf55a532bf
moling <365024424@qq.com>
2018-03-05 18:05:08 +0800

    重构Admin组件

--- src/router-components/Admin.vue
+++ src/router-components/Admin.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="Admin">Admin</div>
+  <div class="admin container">
+    <div class="row">
+      <div class="col-md-3">
+        <sidebar/>
+      </div>
+      <div class="col-md-9 pull-right">
+        <keep-alive>
+          <router-view/>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Sidebar from '@/components/Sidebar'
+
 export default {
   name: 'Admin',
-  data () {
-    return {
-    }
+  components: {
+    Sidebar
   }
 }
 </script>
 
 <style lang="css" scoped>
+.admin {
+  margin-top: 50px;
+}
 </style>
