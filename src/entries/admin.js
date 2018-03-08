06a062a9ef13d3fd4bb711dbb8e5133dd247f95e
moling <365024424@qq.com>
2018-03-08 10:14:34 +0800

    添加应用初始化前的Action

--- src/entries/admin.js
+++ src/entries/admin.js
@@ -8,7 +8,7 @@ import i18n from '@/utils/i18n'
 Vue.config.productionTip = false
 Vue.filter('i18n', i18n)
 
-const actions = ['getConfig']
+const actions = ['getConfig', 'getAllCategories', 'getAllAlbums']
 swal({
   title: i18n('input password'),
   type: 'info',
