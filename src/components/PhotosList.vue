4e7ab7b1fcb4a98ea0f67b2becac9e1b357bde09
moling <365024424@qq.com>
2018-03-10 15:28:11 +0800

    添加PhotosList组件

---
+++ src/components/PhotosList.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="album-photos">
+    <img v-for="photo in photos" :key="photo" :data-src="photo | thumbnail" class="album-photo lazyload">
+  </section>
+</template>
+
+<script>
+import 'lazysizes/lazysizes.min'
+export default {
+  name: 'PhotosList',
+  props: ['photos']
+}
+</script>
+
+<style lang="css" scoped>
+#album-photos {
+  width: 100%;
+  background: white;
+}
+.album-photo {
+  width: 100%;
+  margin-top: .8rem;
+}
+@media only screen and (min-width: 500px) {
+  #album-photos {
+    width: 63.75vw;
+  }
+}
+</style>
